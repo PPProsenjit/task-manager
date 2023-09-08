fb3a1aeb9c017c0ec8b52693a00761dfde18669f
PPProsenjite <prosenjitpappu03455@gmial.com>
2023-09-08 21:35:44 +0600

    done

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Login from '../Login/Login';
+import { getUserLoginEmail } from '../../utilities/Database';
+import UserTask from '../UserTask/UserTask';
+import Tasks from '../Tasks/Tasks';
 
 const Home = () => {
-    return (
-        <div>
-          <Login></Login>  
-        </div>
-    );
+  const userEmail = getUserLoginEmail()
+  return (
+    <div>
+      {
+        userEmail ?
+          <>
+            {
+              (userEmail === "admin@gmail.com") ? <Tasks></Tasks> : <UserTask></UserTask>
+            }
+          </>
+          :
+          <Login></Login>
+      }
+
+    </div>
+  );
 };
 
 export default Home;
